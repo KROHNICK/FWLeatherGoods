9720e0f764aa92e198136c573333eff883ec776d
KROHNICK <business.kanekperez@gmail.com>
2019-10-11 11:39:51 -0700

    created nav component, imported to home component

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
+import Nav from "../Nav/nav";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>FW Leather Goods</h1>
+        <Nav />
       </div>
     );
   }
