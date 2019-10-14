d83a2574dca40363f80ff2041793be82f7d8bf30
KROHNICK <business.kanekperez@gmail.com>
2019-10-14 12:44:32 -0700

    created contact folder, component and view

---
+++ client/src/components/Contact/contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Nav from "../Nav/nav";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="conatiner">
+        <h1>Contact Page</h1>
+        <Nav />
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ client/src/views/ContactView.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Contact from "../components/Contact/contact";
+
+function ContactView(props) {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+}
+
+export default Contact;
